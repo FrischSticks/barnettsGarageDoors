91775bafb84be8f50b391d62bec3bf85d707fb97
Evan Frische <FrischeEvan@gmail.com>
2026-01-07 14:37:47 -0500

    Add Global Colors (Tailwind)

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // The new PostCSS plugin
     autoprefixer: {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,26 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          DEFAULT: '#4BABF5', // light blue
+        },
+        secondary: {
+          light: '#4B5563', // steel gray
+          DEFAULT: '#1F2A37', // navy blue
+          dark: '#1F2A37', //navy blue
+        },
+        accent: {
+          DEFAULT: '#6B7280', // muted slate blue
+        },
+        neutral: {
+          background: '#F3F4F6',
+          DEFAULT: '#F3F4F6',
+          offwhite: '#FFFFFF',
+        },
       },
     },
   },
